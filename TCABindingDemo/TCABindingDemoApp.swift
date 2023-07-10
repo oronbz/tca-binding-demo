b7a89d57334545c1c9e00a9f318bf1fa1b33ea7f
Oron Ben Zvi <oronbz@gmail.com>
2023-07-10 15:04:16 +0300

    Initial Commit

---
+++ TCABindingDemo/TCABindingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCABindingDemoApp.swift
+//  TCABindingDemo
+//
+//  Created by Oron Ben Zvi on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TCABindingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
