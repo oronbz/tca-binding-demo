e60688b7b9760435492ae2947d7128bcf65fda40
Oron Ben Zvi <oronbz@gmail.com>
2023-07-10 15:22:23 +0300

    Demo time

--- TCABindingDemo/ContentView.swift
+++ TCABindingDemo/ContentView.swift
@@ -6,21 +6,58 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+
+struct Feature: Reducer {
+    struct State: Equatable {
+        @BindingState var isSheetPresented = false
+    }
+    
+    enum Action: Equatable, BindableAction {
+        case buttonTapped
+        case binding(BindingAction<State>)
+    }
+    
+    var body: some ReducerOf<Feature> {
+        BindingReducer()
+        Reduce { state, action in
+            switch action {
+            case .buttonTapped:
+                state.isSheetPresented = true
+                return .none
+            case .binding:
+                return .none
+            }
+        }
+    }
+}
 
 struct ContentView: View {
+    let store: StoreOf<Feature>
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        WithViewStore(store, observe: { $0 }) { viewStore in
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+                Button("Click me!") {
+                    viewStore.send(.buttonTapped)
+                }
+            }
+            .sheet(isPresented: viewStore.binding(\.$isSheetPresented)) {
+                Text("Hello from sheet")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(store: .init(initialState: .init()) {
+            Feature()
+        })
     }
 }
--- TCABindingDemo/TCABindingDemoApp.swift
+++ TCABindingDemo/TCABindingDemoApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct TCABindingDemoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(store: .init(initialState: .init()) {
+                Feature()
+            })
         }
     }
 }
